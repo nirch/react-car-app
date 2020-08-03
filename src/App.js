1e399f1ead52bb5c86eb3afb1a90f8fff7f22cc9
nirch <nirh2@yahoo.com>
2020-08-03 11:46:52 +0300

    CarsView now renders a table with all the car data Using bootstrap CDN links and classes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import CarsView from './components/CarsView/CarsView';
 
 function App() {
 
-  const myCar = new CarModel("Toyota", "Yaris", 2002, 230000);
-  console.log(myCar);
-  console.log(myCar.kmPerYear());
+  const carsData = [];
+  carsData.push(new CarModel("Toyota", "Yaris", 2002, 230000));
+  carsData.push(new CarModel("Toyota", "Corola", 2015, 105000));
+  carsData.push(new CarModel("Hyundai", "i30", 2010, 150000));
 
   return (
     <div>
-      <CarsView/>
+      <CarsView cars={carsData}/>
     </div>
   );
 }
