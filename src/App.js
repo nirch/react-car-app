229448d543dbc28125253f5710f687cb1cc014ae
nirch <nirh2@yahoo.com>
2020-08-03 11:26:04 +0300

    adding CarModel to data-model adding the class with the kmPerYear method Tested that class can be imported and tested instance creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CarModel from './data-model/CarModel'
 
 function App() {
+
+  const myCar = new CarModel("Toyota", "Yaris", 2002, 230000);
+  console.log(myCar);
+  console.log(myCar.kmPerYear());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Car App
     </div>
   );
 }
---
+++ src/data-model/CarModel.js
@@ -0,0 +1,18 @@
+
+
+class CarModel {
+    constructor(brand, model, year, km) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.km = km;
+    }
+
+    kmPerYear() {
+        const currentYear = new Date().getFullYear();
+        const age = currentYear - this.year + 1;
+        return this.km / age;
+    }
+}
+
+export default CarModel;
