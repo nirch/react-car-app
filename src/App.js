91cc04e00f618334ff204a888aea686ae7228cd0
nirch <nirh2@yahoo.com>
2020-08-05 12:17:21 +0300

    hard coded cars array is now state in App

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import './App.css';
 import CarModel from './data-model/CarModel'
 import CarsView from './components/CarsView/CarsView';
 
-function App() {
+class App extends React.Component {
 
-  const carsData = [];
-  carsData.push(new CarModel("Toyota", "Yaris", 2002, 230000));
-  carsData.push(new CarModel("Toyota", "Corola", 2015, 105000));
-  carsData.push(new CarModel("Hyundai", "i30", 2010, 150000));
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <CarsView cars={carsData}/>
-    </div>
-  );
+    const carsData = [];
+    carsData.push(new CarModel("Toyota", "Yaris", 2002, 230000));
+    carsData.push(new CarModel("Toyota", "Corola", 2015, 105000));
+    carsData.push(new CarModel("Hyundai", "i30", 2010, 150000));
+
+    this.state = {
+      cars: carsData
+    }
+  
+  }
+
+
+  render() {
+    return (
+      <div>
+        <CarsView cars={this.state.cars}/>
+      </div>
+    );
+  
+  }
 }
 
 export default App;
