c8464dafedafdc71d1d481ec8a8d451041b71765
nirch <nirh2@yahoo.com>
2020-08-09 10:49:54 +0300

    reading the id from the route in CarDetails

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function CarDetails() {
+
+    const { id } = useParams();
+    // const id = window.location.href.split("/")[window.location.href.split("/").length - 1];
+
     return (
        <div>
-           car details...
+           car details of {id}
        </div>
     )
 }
