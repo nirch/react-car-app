f105b0ca81bde518faa8c9c6b5581ad07bb3fd31
nirch <nirh2@yahoo.com>
2020-08-31 10:38:55 +0300

    adding dynamic markers getting long lat from gecode API

--- src/components/CarsView/CarsView.js
+++ src/components/CarsView/CarsView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container, Table} from 'react-bootstrap'
 import { Redirect } from 'react-router-dom';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import Axios from 'axios';
 
 
 class CarsView extends React.Component {
@@ -13,10 +14,32 @@ class CarsView extends React.Component {
         //  in order to decided if to redirect or not. If value is -1 not redirecting.
         //  otherwise redirecting to '/cars/${redirectIndex}'
         this.state = {
-            redirectIndex: -1
+            redirectIndex: -1,
+            locations: []
         }
 
         this.openCar = this.openCar.bind(this);
+    }
+
+    componentDidMount() {
+
+        let geoCodePromises = [];
+        this.props.cars.forEach(car => {
+            const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${car.address}&key=AIzaSyAi5g_KBXE7z_IVdXbbr3oJV3E8YUn7Fec`
+            geoCodePromises.push(Axios.get(geocodeURL));
+            console.log(geocodeURL);
+        })
+
+        Promise.all(geoCodePromises).then(responses => {
+            let newLocations = [];
+            responses.forEach(response => {
+                console.log(response.data);
+                newLocations.push(response.data.results[0].geometry.location)
+            });
+            this.setState({
+                locations: newLocations
+            })
+        })
     }
 
 
@@ -84,7 +107,7 @@ class CarsView extends React.Component {
 
 
         let googleMaps = null;
-        if (this.props.cars && this.props.cars.length >= 1) {
+        if (this.state.locations && this.state.locations.length >= 1) {
 
             const mapStyles = {
                 width: '600px',
@@ -92,14 +115,15 @@ class CarsView extends React.Component {
             };
 
             
+            const markers = this.state.locations.map(location => <Marker position={location}/>)
+
             googleMaps = 
                 <Map
                 google={this.props.google}
                 zoom={6}
                 style={mapStyles}
                 initialCenter={{ lat: 31.5132291, lng: 36.0781026}}>
-                    <Marker position={{lat: 32.0496356, lng: 34.7861492}}/>
-                    <Marker position={{lat: 31.7767234, lng: 35.2323198}}/>
+                    {markers}
                 </Map>
         }
 
