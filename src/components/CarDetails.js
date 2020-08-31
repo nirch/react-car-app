6aed8d235eeb97c21bbd47d69298876060ac9d4a
nirch <nirh2@yahoo.com>
2020-08-31 09:28:23 +0300

    Google Maps iframe to show car location

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -10,14 +10,26 @@ function CarDetails(props) {
     // const id = window.location.href.split("/")[window.location.href.split("/").length - 1];
 
     return (
-       <div>
-           <p>Model: {car.model}</p>
-           <p>Brand: {car.brand}</p>
-           <p>Year: {car.year}</p>
-           <p>KM: {car.km}</p>
-           <p>KM Per Year: {car.kmPerYear()}</p>
-           <p>Address: {car.address}</p>
-       </div>
+        <div>
+            <p>Model: {car.model}</p>
+            <p>Brand: {car.brand}</p>
+            <p>Year: {car.year}</p>
+            <p>KM: {car.km}</p>
+            <p>KM Per Year: {car.kmPerYear()}</p>
+            <p>Address: {car.address}</p>
+
+
+            <iframe
+                width="600"
+                height="450"
+                frameborder="0"
+                style={{border:0}}
+                src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAi5g_KBXE7z_IVdXbbr3oJV3E8YUn7Fec&q=" + car.address}
+                allowfullscreen
+            >
+            </iframe>
+
+        </div>
     )
 }
 
